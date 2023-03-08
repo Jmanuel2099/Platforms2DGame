3f503562999caff05d9835b146a22caf57593b04
Jose Manuel Marin <jmanuel2099@gmail.com>
2023-03-07 19:24:53 -0500

    Player Movement file

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,12 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2d;
 
     [Header("Move")]
-    // private float horizontalMovement = 0f;
     [SerializeField] private float movementSpeed = 2f;
-    // [SerializeField] private float moveSpeed = 400f;
-    // [Range(0, 0.3)][SerializeField] private float smoothedMovement;
-    // private Vector3  velocityAxisZ = Vector3.zero;
-    // private bool lookingRight = true;
 
     [Header("Jump")]
     [SerializeField] private float jumpSpeed;
@@ -23,22 +18,12 @@ public class PlayerMovement : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // // Update is called once per frame
-    // private void Update()
-    // {
-    //     // Left: -1  Right: 1
-    //     horizontalMovement = Input.GetAxisRaw("Horizontal") * movementSpeed;
-
-    // }
-
     //FixedUpdate method is mainly used for physics change
     private void FixedUpdate()
     {
-        // Move(horizontalMovement * Time.fixedDeltaTime);
         Move();
     }
 
-    // private void Move(float move)
     private void Move()
     {
         if (Input.GetKey("d") || Input.GetKey("right"))
@@ -57,31 +42,7 @@ public class PlayerMovement : MonoBehaviour
         {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
-
-        //     Vector3 targetVelocity = new Vector2(move, rb2d.velocity.y);
-        //     rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, targetVelocity, ref velocityAxisZ, smoothedMovement);
-
-        //     if (move > 0 && !lookingRight)
-        //     {
-        //         rotate();
-
-        //     }else if (move < 0 && lookingRight)
-        //     {
-        //         rotate();
-        //     }
     }
 
-    // private void rotate()
-    // {
-    //     lookingRight = !lookingRight;
-    //     Vector3 scale = transform.localScale;
-    //     scale.x *= -1;
-    //     transform.localScale = scale;
-    // }
-
-    private void Jump()
-    {
-
-    }
 }
 
